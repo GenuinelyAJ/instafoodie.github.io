1cbc05324196032cab8d2201acd111100480dd1c
GenuinelyAJ <genuinelyaj@gmail.com>
2023-01-07 05:10:08 +0400

    2nd Commit
    Fixed recipe.html, fixed some errors and added some comments.

--- Media Devices API.js
+++ Media Devices API.js
@@ -5,26 +5,42 @@ var videoRecord;
 var recordedChunks = [];
 
 function toggleRecording() {
-  navigator.mediaDevices.getUserMedia(constraints)
-      .then(gotMedia)
-      .catch(e => { console.error('getUserMedia() failed: ' + e); });
+ 	navigator.mediaDevices.getUserMedia(constraints)
+		.then(gotMedia)
+		.catch(e => { console.error('getUserMedia() failed: ' + e); });
 }
 
 function gotMedia(stream) {
-  videoStream = stream;
-  var video = document.querySelector('video');
-  video.srcObject = stream;
-  try {
-    recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
-  } catch (e) {
+	videoStream = stream;
+  	var video = document.querySelector('video');
+  	video.srcObject = stream;
+  	try {
+    	recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
+  	} catch (e) {
     console.error('Exception while creating MediaRecorder: ' + e);
     return;
-  }
+}
   
-  videoRecord = recorder;
-  recorder.ondataavailable = 
-      (event) => { recordedChunks.push(event.data); };
-  recorder.start(100);
+videoRecord = recorder;
+recorder.ondataavailable = 
+	(event) => { recordedChunks.push(event.data); };
+	recorder.start(100);
+}
+
+function toggleDownload() {
+  	videoRecord.stop();
+  	videoStream.getTracks().forEach(track => { track.stop(); });
+
+  	var blob = new Blob(recordedChunks, {type: "video/webm"});
+  	var url =  URL.createObjectURL(blob);
+  	var a = document.createElement("a");
+  	document.body.appendChild(a);
+  	a.style = "display: none";
+  	a.href = url;
+  	a.download = 'Food Vlog.webm';
+  	a.click();
+  	// setTimeout() here is needed for Firefox.
+  	setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
 }
 
 function getFullscreenElement() {
@@ -44,7 +60,7 @@ function toggleFullscreen() {
 		{
 			document.getElementById("video").requestFullscreen().catch(console.log);
 		}
-	}
+}
 
 	document.addEventListener("dblclick", () => {
 		toggleFullscreen();
@@ -53,20 +69,3 @@ function toggleFullscreen() {
 	document.addEventListener("fullscreenchange", () => {
 		console.log("full screen changed!");
 });
-
-
-function toggleDownload() {
-  videoRecord.stop();
-  videoStream.getTracks().forEach(track => { track.stop(); });
-
-  var blob = new Blob(recordedChunks, {type: "video/webm"});
-  var url =  URL.createObjectURL(blob);
-  var a = document.createElement("a");
-  document.body.appendChild(a);
-  a.style = "display: none";
-  a.href = url;
-  a.download = 'Food Vlog.webm';
-  a.click();
-  // setTimeout() here is needed for Firefox.
-  setTimeout(function() { URL.revokeObjectURL(url); }, 100); 
-}
